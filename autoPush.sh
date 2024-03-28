3a28d34cf8f2fac7abb1e08e0d5affd5c8857f6a
fanhaodong <1007435098@qq.com>
2024-03-28 18:18:18 +0800

    22892

--- autoPush.sh
+++ autoPush.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
- 
+ random_number=$RANDOM
+
+git add .
+git commit -m ${random_number}
+
+# 执行git push直至成功
+echo "启动"
 while true; do
-  if git push; then
-    echo "Push successful"
-    break
-  else
-    echo "Push failed, retrying..."
-  fi
+    # 尝试执行git push
+    git push
+
+    # 检查git push的退出状态
+    if [ $? -eq 0 ]; then
+        echo "Git push 成功"
+        break
+    else
+        echo "Git push 失败，稍后重试..."
+        sleep 5  # 等待5秒后重试
+    fi
 done
+
+echo "结束"
+exit
