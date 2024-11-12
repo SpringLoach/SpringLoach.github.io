edee681d4ecc13f848876a5ef612e6267a61f5f0
fanhaodong <1007435098@qq.com>
2024-11-12 16:29:45 +0800

    11670

--- shell/autoPush.sh
+++ shell/autoPush.sh
@@ -6,8 +6,25 @@ cd /d/power/SpringLoach.github.io
 git add .
 git commit -m ${random_number}
 
+echo "==========================拉取代码=========================="
+# 执行git pull直至成功
+while true; do
+    # 尝试执行git pull
+    echo "尝试执行git pull..."
+    git pull
+
+    # 检查git push的退出状态
+    if [ $? -eq 0 ]; then
+        echo "git pull 成功"
+        break
+    else
+        echo "git pull 失败，即将重试..."
+        sleep 1  # 等待1秒后重试
+    fi
+done
+
 # 执行git push直至成功
-echo "==========================启动=========================="
+echo "==========================拉取代码成功，开始推送代码=========================="
 while true; do
     # 尝试执行git push
     echo "尝试执行git push..."
